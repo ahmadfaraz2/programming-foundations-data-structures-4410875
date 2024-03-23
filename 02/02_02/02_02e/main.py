c7e6be37a05e64ddaec13c3f8f33037ffe55f0dd
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-23 21:32:46 +0500

    Previous method of computing the average number of pets each student has.

--- 02/02_02/02_02e/main.py
+++ 02/02_02/02_02e/main.py
@@ -1,11 +1,17 @@
-''' 
+""" 
 Problem Statement: Compute the average number of pets each student 
 has in a given class. 
-'''
+"""
 
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
 NUM_OF_STUDENTS = len(student_pet_count_list)
 print(NUM_OF_STUDENTS)
 
+
+total_pets = sum(student_pet_count_list)
+print(total_pets)
+
 # average = sum / number of items
+average = total_pets / NUM_OF_STUDENTS
+print(average)
