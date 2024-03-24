31cc78c1ee928a1fb322d8bad5d16d30c5251a03
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-24 22:21:10 +0500

    Learn about multi-dimensional lists and retrive data indexing

--- 02/02_05/02_05b/main.py
+++ 02/02_05/02_05b/main.py
@@ -2,5 +2,21 @@ seating_chart = [
     ["Sarah", "CLaire", "Ben", "Taylor", "Eva"],
     ["Frankie", "George", "Lindsey", "Izzy", "Jack"],
     ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
-    ["Chad", "April", "Matt", "Thomas", "Penny"]
+    ["Chad", "April", "Matt", "Thomas", "Penny"],
 ]
+
+# access Lauren
+print(seating_chart[2][1])
+
+"""
+enumerate( iteratable ) function allow us to loop through the list
+while giving us access to a counter and the value of each item in the iterable.
+"""
+
+for i, row in enumerate(seating_chart):
+    print(f"Row {i+1} contain {row}")
+
+
+for i, row in enumerate(seating_chart):
+    for j, student in enumerate(row):
+        print(f"{student} sits in row {i+1} at seat {j+1}")
--- 02/02_05/02_05e/main.py
+++ 02/02_05/02_05e/main.py
@@ -2,7 +2,7 @@ seating_chart = [
     ["Sarah", "CLaire", "Ben", "Taylor", "Eva"],
     ["Frankie", "George", "Lindsey", "Izzy", "Jack"],
     ["Katherine", "Lauren", "Mary", "Nathan", "Olive"],
-    ["Chad", "April", "Matt", "Thomas", "Penny"]
+    ["Chad", "April", "Matt", "Thomas", "Penny"],
 ]
 
 # print(seating_chart[2][1])
