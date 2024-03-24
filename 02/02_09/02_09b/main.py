43df0290b3f6f0e1251b3a8468ce87f2d5726bcc
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-24 23:33:41 +0500

    Complete the challenge with the little help, greate learning experience

--- 02/02_09/02_09b/main.py
+++ 02/02_09/02_09b/main.py
@@ -1,4 +1,27 @@
 def find_second_smallest(my_list):
-    return 0
+    if len(my_list) < 2:
+        return None
+    sorted_list = sorted(my_list)
+    return sorted_list[1]
+    # for element in sorted_list:
+    #     if element == sorted_list[1]:
+    #         return element
+    # return "You're again wrong!"
 
-print(find_second_smallest([5, 8, 3, 2, 6]))
+
+def find_second_smallest_v2(my_list):
+    if len(my_list) < 2:
+        return None
+
+    smallest_num = float("inf")
+    second_smallest_num = float("inf")
+    for num in my_list:
+        if num < smallest_num:
+            second_smallest_num = smallest_num
+            smallest_num = num
+        elif num < second_smallest_num:
+            second_smallest_num = num
+    return second_smallest_num
+
+
+print(find_second_smallest_v2([5, 8, 3, 2, 6]))
