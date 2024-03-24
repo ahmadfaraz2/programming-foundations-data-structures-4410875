e3c0add45993a648137506837dc555967af6f8e6
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-24 22:53:36 +0500

    Learn about sorting

--- 02/02_08/02_08b/main.py
+++ 02/02_08/02_08b/main.py
@@ -1,2 +1,9 @@
 my_list = [1, 7, 3]
-print(sorted(my_list))
+print(sorted(my_list, reverse=True))
+
+
+student_grade = [("Ahmad", 85), ("Zahid", 82), ("Saad", 91), ("Hamza", 89)]
+
+print(sorted(student_grade))
+print(sorted(student_grade, key=lambda x: x[1], reverse=True))
+print(sorted(student_grade, key=lambda x: x[1], reverse=False))
