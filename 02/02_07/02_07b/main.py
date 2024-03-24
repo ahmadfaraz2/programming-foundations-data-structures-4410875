53e1e86a5bf2975d489781ff02ce0fc47ded09a4
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-24 22:41:12 +0500

    Learn about how to search an item from any list, Perform Linear Search

--- 02/02_07/02_07b/main.py
+++ 02/02_07/02_07b/main.py
@@ -1,1 +1,25 @@
 # Linear Search
+
+"""
+It is also called Squential Search, it is easy but it is so slow if there is 
+large amount of data.
+"""
+
+
+my_list = [5, 3, 1, 6, 2, 7, 8]
+
+item = 6
+
+
+def search(item, listy):
+    for element in listy:
+        if element == item:
+            return True
+    return False
+
+
+print(search(item, listy=my_list))
+
+
+item_index = my_list.index(item)  # Also a Linear Search
+print(item_index)
