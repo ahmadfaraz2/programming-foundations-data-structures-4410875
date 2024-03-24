156d02828b448dc6436c3aedb7b09dd56c281870
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-24 22:29:22 +0500

    Learn about tuples and how to access tuples values and also a function can return tuple, basically anything

--- 02/02_06/02_06b/main.py
+++ 02/02_06/02_06b/main.py
@@ -1,1 +1,17 @@
 # Tuples are immutable array-like structures
+
+point = (2, 5)
+
+print(point[0])
+print(point[1])
+
+
+def calculate_square_prop(side_length):
+    area = side_length * side_length
+    perimeter = 4 * side_length
+    return (area, perimeter)
+
+
+result = calculate_square_prop(3)
+print("Area: ", result[0])
+print("Perimeter: ", result[1])
