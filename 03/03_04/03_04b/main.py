a558c92ac8b3dd65d73bcf8048c3ab94becebc08
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-25 23:52:16 +0500

    Completed the challenge and learn about dictionary comperihenions

--- 03/03_04/03_04b/main.py
+++ 03/03_04/03_04b/main.py
@@ -3,12 +3,28 @@ user_preferences = {
     "language": "English",
     "notifications": None,
     "currency": "USD",
-    "theme": None
+    "theme": None,
 }
 
 
-def update_preferences(user_pref):
-    return {}
+# def update_preferences(user_pref):
+#     for key, value in user_pref.items():
+#         if value is None:
+#             del user_pref[key]
+#     return {user_pref}
+
+
+def update_preferences(user_perf):
+    updated_preferences = {}
+    for key, value in user_perf.items():
+        if value is not None:
+            updated_preferences[key] = value
+    return updated_preferences
+
+
+def update_preferences_v2(user_perf):
+    return {key: value for key, value in user_perf.items() if value is not None}
 
 
 print(update_preferences(user_preferences))
+print(update_preferences_v2(user_preferences))
