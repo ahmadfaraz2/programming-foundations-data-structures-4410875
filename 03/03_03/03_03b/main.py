91e350d8f466f64896a429cd45679ad0eec85092
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-25 23:26:25 +0500

    Preform different operartions (functinos) on dictionary

--- 03/03_03/03_03b/main.py
+++ 03/03_03/03_03b/main.py
@@ -5,5 +5,22 @@ user_preferences = {
     "currency": "USD",
     "enable_location": False,
     "volume_level": 80,
-    "date_format": "MM/DD/YYYY"
+    "date_format": "MM/DD/YYYY",
 }
+
+
+# changing keys values
+user_preferences["language"] = "Spanish"
+
+user_preferences["volume_level"] = 43
+
+# Adding new keys and values
+user_preferences["highlight_color"] = "Blue"
+
+# Deleting for dictionary
+del user_preferences["currency"]  # does not return value
+
+removed_item = user_preferences.pop("date_format", "N/A")
+
+
+print(user_preferences)
