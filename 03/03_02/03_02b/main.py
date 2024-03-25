94fa4e0b808a7a8b21455dcc43fd6bafbb389451
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-25 23:07:08 +0500

    Learn about Dictionary data structure

--- 03/03_02/03_02b/main.py
+++ 03/03_02/03_02b/main.py
@@ -1,2 +1,21 @@
 # Key: State
 # Value: Capital
+
+states_to_capital = {
+    "New York": "Albony",
+    "Texas": "Austin",
+}
+
+
+print(states_to_capital["Texas"])
+print("~~~~~~~~~~~~~~~~~~~~")
+
+# Retrive keys
+for key in states_to_capital.keys():
+    print(key)
+print("~~~~~~~~~~~~~~~~~~~~")
+
+
+# Retrive keys and values
+for key, value in states_to_capital.items():
+    print(key + " | " + value)
