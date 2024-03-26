c8a9e99f3dd9942e389306db7946d5e71f5b23b8
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-26 21:24:52 +0500

    Compeleted the sets challenge

--- 04/04_05/04_05b/main.py
+++ 04/04_05/04_05b/main.py
@@ -1,7 +1,14 @@
 def has_unique_characters(data):
-    return False
+    unique = set(data)
+    return len(unique) == len(data)
 
-print(has_unique_characters('sample'))
-print(has_unique_characters('hello world'))
-print(has_unique_characters('linkedin'))
-print(has_unique_characters('python'))
+    # if len(unique) == len(data):
+    #     return True
+    # else:
+    #     return False
+
+
+print(has_unique_characters("sample"))
+print(has_unique_characters("hello world"))
+print(has_unique_characters("linkedin"))
+print(has_unique_characters("python"))
