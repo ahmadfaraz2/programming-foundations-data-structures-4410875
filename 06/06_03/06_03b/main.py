218e7b79975d30da43efd2368f80dc5230d648f8
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2024-03-26 23:34:23 +0500

    Learn about how to use deque as Stack

--- 06/06_03/06_03b/main.py
+++ 06/06_03/06_03b/main.py
@@ -1,1 +1,12 @@
 from collections import deque
+
+history_stack = deque()
+
+
+history_stack.append("https://google.com")  # bottom
+history_stack.append("https://linkedin.com")
+history_stack.append("https://stackoverflow.com")  # top
+
+
+print(history_stack[-1])
+print(history_stack.pop())
